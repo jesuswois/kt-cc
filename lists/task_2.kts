55dff5ed4e9368cd5accc9d527aa9f5fbda829aa
Jesús Domínguez Ramírez <jesus.dominguez.ramirez855@gmail.com>
2026-06-24 14:12:13 -0600

    Lists Chapter 2nd Task

--- lists/task_2.kts
+++ lists/task_2.kts
@@ -2,3 +2,17 @@
 //
 //Count the number of times a given element occurs in an `IntList`.
 //
+
+main()
+
+fun main(){
+    countInt(5,listOf(1,5,2,3,2,1,5,5,0,2,3,4,0))
+}
+
+fun countInt(int: Int, intList: List<Int>): Unit{
+    var matches = 0
+    for(i in intList){
+        if(i==int) matches++
+    }
+    println("$int appears $matches times in the list.")
+}
