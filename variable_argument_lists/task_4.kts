9962b1c58b75fc314bb078f54ddef27d0f480bf5
Jesús Domínguez Ramírez <jesus.dominguez.ramirez855@gmail.com>
2026-07-01 14:05:35 -0600

    Variable Argument List Section's 4th Task

--- variable_argument_lists/task_4.kts
+++ variable_argument_lists/task_4.kts
@@ -4,3 +4,20 @@
 //        exercise). This function is called on a `List` of `List`s and returns a single
 //        `List` of all elements from the nested `List`s. Implement `flatten()` without
 //using the `flatten()` function from the standard library.
+
+main()
+
+fun main(){
+    val flattenList = flatten(listOf(listOf("what","bye"),listOf("him","water"),listOf("red")))
+    println(flattenList)
+}
+
+fun flatten(chunked: List<List<String>>): List<String> {
+    val results = mutableListOf<String>()
+    for (chunk in chunked){
+        for(piece in chunk){
+            results.add(piece)
+        }
+    }
+    return results
+}
